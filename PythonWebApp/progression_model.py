d05a4f2d7a640a60be298ff905f92dd882ecfa23
ayaanzhaque <ayaanzhaque@gmail.com>
2020-06-14 17:31:21 -0700

    progression web app

--- PythonWebApp/progression_model.py
+++ PythonWebApp/progression_model.py
@@ -157,4 +157,35 @@ myPalette = sns.xkcd_palette(colors)
 plt.figure(figsize = (15, 10), dpi=300)
 plt.title("Classification of entries in Victoria's Diary\n", fontsize=14)
 ax = sns.barplot(y='vic_detail', x='counts', data=vic_plot_df, hue='predicted_suicide', palette=myPalette, errwidth=0.01);
-plt.ylabel("category of entries");
+plt.ylabel("category of entries")
+
+def prediction(model, text):
+    text_array = pd.Series(text)
+    processed_text = processing_text(text_array)
+
+    processed_array = pd.Series(processed_text)
+
+    tvec_optimised = TfidfVectorizer(max_features=70, ngram_range=(1, 3),stop_words = 'english')
+    processed_text_tvec = tvec_optimised.fit_transform(processed_array).todense()
+
+    prediction = model.predict(processed_text_tvec)
+
+    return(prediction[0])
+
+def processing_text(series_to_process):
+    new_list = []
+    tokenizer = RegexpTokenizer(r'(\w+)')
+    lemmatizer = WordNetLemmatizer()
+
+    for i in range(len(series_to_process)):
+        # tokenized item in a new list
+        dirty_string = (series_to_process)[i].lower()
+        words_only = tokenizer.tokenize(dirty_string) # words_only is a list of only the words, no punctuation
+        #Lemmatize the words_only
+        words_only_lem = [lemmatizer.lemmatize(i) for i in words_only]
+        # removing stop words
+        words_without_stop = [i for i in words_only_lem if i not in stopwords.words("english")]
+        # return seperated words
+        long_string_clean = " ".join(word for word in words_without_stop)
+        new_list.append(long_string_clean)
+        return new_list
